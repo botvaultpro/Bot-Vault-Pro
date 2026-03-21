af8a7ff9421a6e7d2976108ba6a7f32b96c460a5
Bot Vault Pro <botvaultpro@outlook.com>
2026-03-21 11:47:43 -0500

    Change test directory to root

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './bvp-tests',
+  testDir: './',
   timeout: 60000,
   use: {
     baseURL: 'https://botvaultpro.com',
